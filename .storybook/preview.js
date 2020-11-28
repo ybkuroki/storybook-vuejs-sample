f7c20691197228ea37295ee316f7d4816926a9ad
ybkuroki <45133652+ybkuroki@users.noreply.github.com>
2020-11-28 17:33:32 +0900

    Upgrade Storybook v6.1.8

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,14 +1,11 @@
-import { addParameters, addDecorator } from '@storybook/vue';
 import Vue from 'vue';
 import Vuex from 'vuex';
-import { withA11y } from '@storybook/addon-a11y';
 
-addDecorator(withA11y);
 Vue.use(Vuex);
 
-addParameters({
+export const parameters = {
   docs: {
     inlineStories: true,
     iframeHeight: '60px',
   },
-});
+};
